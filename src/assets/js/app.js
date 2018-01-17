7d513eb948509416cc3f44f1add40886d71cbd50
Misha Pliskunov <pliskunov.mykhailo@gmail.com>
2018-01-17 19:00:29 +0200

    init

---
+++ src/assets/js/app.js
@@ -0,0 +1,236 @@
+//=require jquery.min.js
+
+// to inlude bootstrap plugins add "=" sign below
+// require javascripts/bootstrap/*.*
+//=require javascripts/bootstrap.min.js
+
+//=require slick.min.js
+
+;(function($){
+
+	$(document).ready(function(){
+		var $slider = $('.ba-slider');
+
+		$slider.slick({
+			autoplay: true,
+			autoplaySpeed: 3000,
+			arrows: false,
+			dots: true,
+			slide: '.ba-slide'
+		});
+	});
+
+	function initMap() {
+
+		var mapBlock = document.querySelector('.ba-map'),
+			address = mapBlock.innerHTML;
+
+		var poltava = {
+			lat: 49.588267,
+			lng: 34.551417
+		};
+
+		var baMap = new google.maps.Map(mapBlock, {
+			zoom: 12,
+			center: poltava,
+			disableDefaultUI: true,
+			styles: [
+				{
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"color": "#f5f5f5"
+						}
+					]
+				},
+				{
+					"elementType": "labels.icon",
+					"stylers": [
+						{
+							"visibility": "off"
+						}
+					]
+				},
+				{
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#616161"
+						}
+					]
+				},
+				{
+					"elementType": "labels.text.stroke",
+					"stylers": [
+						{
+							"color": "#f5f5f5"
+						}
+					]
+				},
+				{
+					"featureType": "administrative.land_parcel",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#bdbdbd"
+						}
+					]
+				},
+				{
+					"featureType": "poi",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"color": "#eeeeee"
+						}
+					]
+				},
+				{
+					"featureType": "poi",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#757575"
+						}
+					]
+				},
+				{
+					"featureType": "poi.park",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"color": "#e5e5e5"
+						}
+					]
+				},
+				{
+					"featureType": "poi.park",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#9e9e9e"
+						}
+					]
+				},
+				{
+					"featureType": "road",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"color": "#ffffff"
+						}
+					]
+				},
+				{
+					"featureType": "road.arterial",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#757575"
+						}
+					]
+				},
+				{
+					"featureType": "road.highway",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"color": "#dadada"
+						}
+					]
+				},
+				{
+					"featureType": "road.highway",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#616161"
+						}
+					]
+				},
+				{
+					"featureType": "road.local",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#9e9e9e"
+						}
+					]
+				},
+				{
+					"featureType": "transit.line",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"color": "#e5e5e5"
+						}
+					]
+				},
+				{
+					"featureType": "transit.station",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"color": "#eeeeee"
+						}
+					]
+				},
+				{
+					"featureType": "water",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"color": "#c9c9c9"
+						}
+					]
+				},
+				{
+					"featureType": "water",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#9e9e9e"
+						}
+					]
+				}
+			]
+		});
+
+		var marker = new google.maps.Marker({
+          position: poltava,
+          map: baMap,
+          animation: google.maps.Animation.DROP,
+          icon: './assets/img/marker.png'
+				});
+				
+				google.maps.event.addDomListener(window, "resize", function() {
+					var center = baMap.getCenter();
+					google.maps.event.trigger(baMap, "resize");
+					baMap.setCenter(center);
+			});
+	}
+        window.onload = initMap;
+
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
